c58df7a3d9c5497c0f2083ff7049adccfb6a2ae1
Lirusaito <inusaito@gmail.com>
2019-03-13 17:52:05 -0400

    Make the viewer lighter by one or two BOOLs per slider!

--- indra/llui/llsliderctrl.cpp
+++ indra/llui/llsliderctrl.cpp
@@ -59,7 +59,6 @@ LLSliderCtrl::LLSliderCtrl(const std::string& name, const LLRect& rect,
 						   S32 text_left,
 						   BOOL show_text,
 						   BOOL can_edit_text,
-						   BOOL volume,
 						   commit_callback_t commit_callback,
 						   F32 initial_value, F32 min_value, F32 max_value, F32 increment,
 						   const std::string& control_which)
@@ -67,7 +66,6 @@ LLSliderCtrl::LLSliderCtrl(const std::string& name, const LLRect& rect,
 	  mFont(font),
 	  mShowText( show_text ),
 	  mCanEditText( can_edit_text ),
-	  mVolumeSlider( volume ),
 	  mPrecision( 3 ),
 	  mLabelBox( NULL ),
 	  mLabelWidth( label_width ),
@@ -105,7 +103,7 @@ LLSliderCtrl::LLSliderCtrl(const std::string& name, const LLRect& rect,
 	mSlider = new LLSlider(std::string("slider"),
 						   slider_rect, 
 						   boost::bind(&LLSliderCtrl::onSliderCommit,_1,_2), 
-						   initial_value, min_value, max_value, increment, volume,
+						   initial_value, min_value, max_value, increment,
 						   control_which );
 	addChild( mSlider );
 	
@@ -387,8 +385,6 @@ LLXMLNodePtr LLSliderCtrl::getXML(bool save_children) const
 	node->createChild("show_text", TRUE)->setBoolValue(mShowText);
 
 	node->createChild("can_edit_text", TRUE)->setBoolValue(mCanEditText);
-
-	node->createChild("volume", TRUE)->setBoolValue(mVolumeSlider);
 	
 	node->createChild("decimal_digits", TRUE)->setIntValue(mPrecision);
 
@@ -437,9 +433,6 @@ LLView* LLSliderCtrl::fromXML(LLXMLNodePtr node, LLView *parent, LLUICtrlFactory
 
 	BOOL can_edit_text = FALSE;
 	node->getAttributeBOOL("can_edit_text", can_edit_text);
-
-	BOOL volume = FALSE;
-	node->getAttributeBOOL("volume", volume);
 
 	F32 initial_value = 0.f;
 	node->getAttributeF32("initial_val", initial_value);
@@ -497,7 +490,6 @@ LLView* LLSliderCtrl::fromXML(LLXMLNodePtr node, LLView *parent, LLUICtrlFactory
 							rect.getWidth() - text_left,
 							show_text,
 							can_edit_text,
-							volume,
 							NULL,
 							initial_value, 
 							min_value, 
