c260158a2c18a4e66dd4035340d95ce05e3d29ce
Lirusaito <inusaito@gmail.com>
2019-03-14 02:23:06 -0400

    Backport translatable not_online responses
    Thanks for pointing this out, Damian

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -2240,6 +2240,20 @@ void autoresponder_finish(bool show_autoresponded, const LLUUID& session_id, con
 	}
 }
 
+const std::string NOT_ONLINE_MSG("User not online - message will be stored and delivered later.");
+const std::string NOT_ONLINE_INVENTORY("User not online - inventory has been saved.");
+void translate_if_needed(std::string& message)
+{
+	if (message == NOT_ONLINE_MSG)
+	{
+		message = LLTrans::getString("not_online_msg");
+	}
+	else if (message == NOT_ONLINE_INVENTORY)
+	{
+		message = LLTrans::getString("not_online_inventory");
+	}
+}
+
 void process_improved_im(LLMessageSystem *msg, void **user_data)
 {
 	if (gNoRender)
@@ -2307,6 +2321,12 @@ void process_improved_im(LLMessageSystem *msg, void **user_data)
 	&& NACLAntiSpamRegistry::checkQueue((U32)NACLAntiSpamRegistry::QUEUE_IM,from_id))
 		return;
 	// NaCl End
+
+	// Singu TODO: LLIMProcessing goes here
+	if (chat.mSourceType == CHAT_SOURCE_SYSTEM)
+	{ // Translate server message if required (MAINT-6109)
+		translate_if_needed(message);
+	}
 
 	// make sure that we don't have an empty or all-whitespace name
 	LLStringUtil::trim(name);
