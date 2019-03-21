879cd79fdc2962422c8e13906abb817d4f6f9afd
Lirusaito <inusaito@gmail.com>
2019-03-21 03:13:13 -0400

    Gesture UI optimization, first pass

--- indra/newview/llpreview.h
+++ indra/newview/llpreview.h
@@ -137,6 +137,8 @@ protected:
 	// for LLInventoryObserver 
 	virtual void changed(U32 mask);	
 	BOOL mDirty;
+
+public:
 	virtual const char *getTitleName() const { return "Preview"; }
 	
 protected:
