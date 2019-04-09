181c6ddd9d9383d7594d2df053e51b591932d978
Lirusaito <inusaito@gmail.com>
2019-04-09 02:08:25 -0400

    LLGroupMgr Don't copy vectors around, we can use references
    getMembersInRole with needs_sort will now sort the vector passed in
    At the moment it's not used with this being true so it doesn't matter
    Just take note.

--- indra/newview/llgroupmgr.cpp
+++ indra/newview/llgroupmgr.cpp
@@ -160,7 +160,7 @@ LLGroupRoleData::~LLGroupRoleData()
 {	
 }
 
-S32 LLGroupRoleData::getMembersInRole(uuid_vec_t members,
+S32 LLGroupRoleData::getMembersInRole(uuid_vec_t& members,
 									  BOOL needs_sort)
 {
 	if (mRoleID.isNull())
@@ -2083,7 +2083,7 @@ void GroupBanDataResponder::httpSuccess()
 void LLGroupMgr::sendGroupBanRequest(	EBanRequestType request_type,
 										const LLUUID& group_id,
 										U32 ban_action, /* = BAN_NO_ACTION */
-										const uuid_vec_t ban_list) /* = uuid_vec_t() */
+										const uuid_vec_t& ban_list) /* = uuid_vec_t() */
 {
 	LLViewerRegion* currentRegion = gAgent.getRegion();
 	if (!currentRegion)
