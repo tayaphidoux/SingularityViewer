b957c0930d25aab5b9bdcc12030df829883b8905
Liru Færs <lirusaito@gmail.com>
2020-01-17 02:27:02 -0500

    Fix dumb in LLSurface, don't do dumb math

--- indra/newview/llsurface.cpp
+++ indra/newview/llsurface.cpp
@@ -167,10 +167,10 @@ void LLSurface::create(const S32 grids_per_edge,
 	mGridsPerEdge = grids_per_edge + 1;  // Add 1 for the east and north buffer
 	mOOGridsPerEdge = 1.f / mGridsPerEdge;
 	mGridsPerPatchEdge = grids_per_patch_edge;
-	mPatchesPerEdge = (mGridsPerEdge - 1) / mGridsPerPatchEdge;
+	mPatchesPerEdge = grids_per_edge / mGridsPerPatchEdge;
 	mNumberOfPatches = mPatchesPerEdge * mPatchesPerEdge;
-	mMetersPerGrid = width / ((F32)(mGridsPerEdge - 1));
-	mMetersPerEdge = mMetersPerGrid * (mGridsPerEdge - 1);
+	mMetersPerGrid = width / (F32)grids_per_edge;
+	mMetersPerEdge = mMetersPerGrid * grids_per_edge;
 // <FS:CR> Aurora Sim
 	sTextureSize = width;
 // </FS:CR> Aurora Sim
