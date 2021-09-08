c6b403b5cb4bd4517604e739f72eee72655afc2b
tayaphidoux <83609871+tayaphidoux@users.noreply.github.com>
2021-09-08 13:19:33 -0600

    support gstreamer 1.0

--- indra/cmake/GStreamer010Plugin.cmake
+++ indra/cmake/GStreamer010Plugin.cmake
@@ -1,8 +1,8 @@
 # -*- cmake -*-
 include(Prebuilt)
   include(FindPkgConfig)
-  pkg_check_modules(GSTREAMER010 REQUIRED gstreamer-0.10)
-  pkg_check_modules(GSTREAMER010_PLUGINS_BASE REQUIRED gstreamer-plugins-base-0.10)
+  pkg_check_modules(GSTREAMER010 REQUIRED gstreamer-1.0)
+  pkg_check_modules(GSTREAMER010_PLUGINS_BASE REQUIRED gstreamer-plugins-base-1.0)
 
 
 if (WINDOWS)
@@ -11,8 +11,8 @@ if (WINDOWS)
     set(GSTREAMER010_LIBRARIES
          libgstvideo
          libgstaudio
-         libgstbase-0.10
-         libgstreamer-0.10
+         libgstbase-1.0
+         libgstreamer-1.0
          gobject-2.0
          gmodule-2.0
          gthread-2.0
@@ -22,10 +22,10 @@ else (WINDOWS)
   # We don't need to explicitly link against gstreamer itself, because
   # LLMediaImplGStreamer probes for the system's copy at runtime.
     set(GSTREAMER010_LIBRARIES
-         gstvideo-0.10
-         gstaudio-0.10
-         gstbase-0.10
-         gstreamer-0.10
+         gstvideo-1.0
+         gstaudio-1.0
+         gstbase-1.0
+         gstreamer-1.0
          gobject-2.0
          gmodule-2.0
          dl
@@ -39,7 +39,7 @@ endif (WINDOWS)
 
 
 if (GSTREAMER010_FOUND AND GSTREAMER010_PLUGINS_BASE_FOUND)
-  set(GSTREAMER010 ON CACHE BOOL "Build with GStreamer-0.10 streaming media support.")
+  set(GSTREAMER010 ON CACHE BOOL "Build with GStreamer-1.0 streaming media support.")
   add_definitions(-DLL_GSTREAMER010_ENABLED=1)
 endif (GSTREAMER010_FOUND AND GSTREAMER010_PLUGINS_BASE_FOUND)
 
